8c4c60cf28676fac274cec65b888568a637c4f6f
Daniel Walley <dwalley606@gmail.com>
2024-03-25 21:45:42 -0400

    Final attempt to add functionality

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# api-challenge
+# Task Board
+
+My first attempt to build a task board to handle daily tasks.
+
+## Screenshot
+
+![127 0 0 1_5500_index html (1)](https://github.com/dwalley606/api-challenge/assets/127894209/eac60d6e-bfb2-46e8-96e3-452e82bbd0cc)
+
+
+##Link
+
+Find deployed application here:
+
+[Click Here](https://dwalley606.github.io/api-challenge/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -72,5 +72,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList(); // Render the task list
+    $(".lane").droppable(); // Make lanes droppable
+    $("#dueDate").datepicker(); // Make the due date field a date picker
 });
